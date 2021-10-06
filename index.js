d77c3c4dea39129e1ce22287c1f6b79629872e60
Oshini1202 <aishcharya.2019757@iit.ac.lk>
2021-10-06 22:30:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+// import redux from "redux";
+const redux = require("redux"); // to run as a small node application
+const reactLogger = require("redux-logger");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+const applyMiddleware = redux.applyMiddleware;
+const logger = ReduxLogger.createLogger();
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+function buyIceCream() {
+  return {
+    type: "BUY_ICECREAM",
+  };
+}
+
+// (previousState , action) => newState
+// const initialState = {
+//   numOfCakes: 10,
+//   numOfIceCreams: 20,
+// };
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case BUY_CAKE:
+//       return {
+//         ...state, // creating a copy of state obj
+//         numOfCakes: state.numOfCakes - 1, // then updating the created copy
+//       };
+//     case BUY_ICECREAM:
+//       return {
+//         ...state, // creating a copy of state obj
+//         numOfIceCreams: state.numOfIceCreams - 1, // then updating the created copy
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+//////////////////////////////
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, // creating a copy of state obj
+        numOfCakes: state.numOfCakes - 1, // then updating the created copy
+      };
+
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state, // creating a copy of state obj
+        numOfIceCreams: state.numOfIceCreams - 1, // then updating the created copy
+      };
+
+    default:
+      return state;
+  }
+};
+
+//////////////////////////////
+// accepts an object
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+// const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log("Initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  //console.log("Update state", store.getState())
+  {}
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+unsubscribe();
